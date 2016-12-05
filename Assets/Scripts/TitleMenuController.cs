6a327a7e33a12d249060645b133aba1e2b05f617
dirtyhobogamesjuho <erkkiha@metropolia.fi>
2016-12-05 14:13:56 +0200

    toimiva menu -juho

--- Assets/Scripts/TitleMenuController.cs
+++ Assets/Scripts/TitleMenuController.cs
@@ -10,6 +10,7 @@ namespace Assets.Scripts {
         private Button AboutButton;
         private Button QuitButton;
         private Button HelpButton;
+        private Button continueButton;
 
         private GameObject ImageToggle;
         private GameObject AboutToggle;
@@ -28,45 +29,69 @@ namespace Assets.Scripts {
             AboutButton = GameObject.Find("About").GetComponent<Button>();
             QuitButton = GameObject.Find("Quit").GetComponent<Button>();
             HelpButton = GameObject.Find("Help").GetComponent<Button>();
+            continueButton = GameObject.Find("HelpToggle/ContinueButton").GetComponent<Button>();
 
             AboutToggle = GameObject.Find("AboutToggle");
             ImageToggle = GameObject.Find("ImageToggle");
             HelpToggle = GameObject.Find("HelpToggle");
 
-            PlayButton.onClick.AddListener(() => startGame());
+            continueButton.onClick.AddListener(() => startGame());
+
+            PlayButton.onClick.AddListener(() => beginGame());
             AboutButton.onClick.AddListener(() => toggleAbout());
             QuitButton.onClick.AddListener(() => Application.Quit());
             HelpButton.onClick.AddListener(() => toggleHelp());
 
             AboutToggle.SetActive(false);
             HelpToggle.SetActive(false);
+            continueButton.gameObject.SetActive(false);
+            
+
+        }
+
+        //Shows the "How to play" text, disactivates Play, How to play, About and Quit buttons, activates Continue button and pauses the music. Pressing Continue starts the game
+
+        void beginGame() {
+            continueButton.gameObject.SetActive(true);
+            PlayButton.gameObject.SetActive(false);
+            AboutButton.gameObject.SetActive(false);
+            QuitButton.gameObject.SetActive(false);
+            HelpButton.gameObject.SetActive(false);
+            QuitButton.gameObject.SetActive(false);
+            ImageToggle.SetActive(false);
+            HelpToggle.SetActive(true);
+            SoundManager.instance.musicSource.Pause();
 
         }
 
         void startGame() {
-
+         
             int currentScene = SceneManager.GetActiveScene().buildIndex;
             SceneManager.LoadScene(currentScene + 1);
 
         }
 
+        //Displays the About text
         void toggleAbout() {
 
             if(AboutToggle.activeSelf == true) {
 
                 ImageToggle.SetActive(true);
                 AboutToggle.SetActive(false);
+                continueButton.gameObject.SetActive(false);
 
 
             } else {
 
                 ImageToggle.SetActive(false);
                 AboutToggle.SetActive(true);
+                continueButton.gameObject.SetActive(false);
 
             }
 
         }
 
+        //Displays the How to play text
         void toggleHelp() {
 
             if (HelpToggle.activeSelf == true) {
